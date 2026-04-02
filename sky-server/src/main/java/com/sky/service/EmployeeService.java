b435d994e4ccf8d4f2ce11933dc34b9d97b0cead
lvesDi005 <2973373005@qq.com>
2026-04-02 12:12:34 +0800

    补全编辑员工:1.根据id查询 2.编辑

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -36,4 +36,13 @@ public interface EmployeeService {
      * @return
      */
     Object startOrStop(Integer status, Long id);
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
